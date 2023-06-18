6984c7e2d631eb3ccf5e61b71b39e6cd36f24b69
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-18 16:20:05 +0530

    [somu]Add:Creating Stack and Adding The Stacks.

---
+++ StacksAndQueuesDataStructures/Program.cs
@@ -0,0 +1,30 @@
+﻿using StacksAndQueuesDataStructures;
+
+namespace StacksAndQueuesDataStructure
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose Option to Perform\n1.Push the Code\n2.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                StacksAndQueues stack=new StacksAndQueues();
+                switch (option)
+                {
+                    case 1:
+                        stack.Push(56);
+                        stack.Push(30);
+                        stack.Push(70);
+                        stack.Display();
+                        break;
+                        case 2:
+                        flag= false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ StacksAndQueuesDataStructures/StacksAndQueues.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueuesDataStructures
+{
+    public class StacksAndQueues
+    {
+        private Node top;
+        public StacksAndQueues()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if(this.top == null)    
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is Pushed to Stack",value);
+        }
+        public void Display()
+        {
+            Node temp=this.top;
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp= temp.next;
+            }
+        }
+    }
+}
