ffbe7674e28c6a0cb17bd20eec0e5a45c00062dd
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-18 17:23:41 +0530

    [somu]Add:Creating Queues and Enqueue to the List.

--- StacksAndQueuesDataStructures/Program.cs
+++ StacksAndQueuesDataStructures/Program.cs
@@ -9,7 +9,7 @@ namespace StacksAndQueuesDataStructure
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose Option to Perform\n1.Push the Code\n2.Peek and Pop\n3.Exit");
+                Console.WriteLine("Choose Option to Perform\n1.Push the Code\n2.Peek and Pop\n3.Enqueu the Queues\n4.Exit");
                 int option=Convert.ToInt32(Console.ReadLine());
                 StacksAndQueues stack=new StacksAndQueues();
                 switch (option)
@@ -28,7 +28,14 @@ namespace StacksAndQueuesDataStructure
                         stack.Pop();
                         stack.Display();
                         break;
-                        case 3:
+                    case 3:
+                        Queues queues=new Queues();
+                        queues.Enqueue(56);
+                        queues.Enqueue(30);
+                        queues.Enqueue(70);
+                        queues.Display();
+                        break;
+                        case 4:
                         flag= false;
                         break;
                 }
